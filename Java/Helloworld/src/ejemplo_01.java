aa60a4a783ca86d03f85d3b2bcb4648a5bcceee7
Jankarlos Crespo <jankarloscrespohdz@gmail.com>
2024-03-06 12:12:49 -0400

    "example_01"

--- Java/Helloworld/src/ejemplo_01.java
+++
@@ -1,9 +0,0 @@
-public class ejemplo_01 {
-    public static void main(String[] args) {
-        int myVariable = 10;
-        final int myConstant = 20;
-        System.out.println("Valor de mi variable: "+ myVariable);
-        System.out.println("Valor de mi constante: "+ myConstant);
-
-    }
-}
