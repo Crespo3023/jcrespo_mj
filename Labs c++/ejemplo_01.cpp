ad970f11d437a601458ef9cce51d1de94ee65931
Jankarlos Crespo <jankarloscrespohdz@gmail.com>
2024-03-07 09:09:46 -0400

    "example_01"

--- Labs c++/ejemplo_01.cpp
+++ Labs c++/ejemplo_01.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
 int main ()
 {
+   
+    int myVariable = 10;
+    const int myConstant = 20;
+    
+    cout <<"Valor de mi variable: "<< myVariable << endl;
+    cout <<"Valor de mi constante: "<< myConstant << endl;
 
-    cout <<"Hello world!!!\n";
 
     return 0;
 
