cf9e2fffca7105816eedddc4fb66c68004070dd4
Jankarlos Crespo <jankarloscrespohdz@gmail.com>
2024-03-07 09:22:10 -0400

    "example_01"

--- Labs Java/Helloworld/hello_world/ejemplo_01.java
+++ Labs Java/Helloworld/hello_world/ejemplo_01.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!!");
+public class ejemplo_01
+{
+
+    public static void main(String[] args) 
+    {
+        int myVariable = 10;
+        final int myConstant = 20;
+
+        System.out.println("Valor de mi variable: "+ myVariable);
+
+        System.out.println("Valor de mi constante: " + myConstant);
+
     }
 }
