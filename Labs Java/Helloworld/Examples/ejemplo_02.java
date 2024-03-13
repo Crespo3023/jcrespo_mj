0480c91c58f883f6dc612d4e6c6999ce1cb35f73
Jankarlos Crespo <jankarloscrespohdz@gmail.com>
2024-03-13 14:10:10 -0400

    "example_02"

---
+++ Labs Java/Helloworld/Examples/ejemplo_02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ejemplo_02
+{
+public static void main(String[] args) {
+    Scanner scanner= new Scanner(System.in);
+
+    System.out.print("Ingrese un numero: ");
+    int entero= scanner. nextInt();
+
+    System.out.print("Ingrese un numero flotante: ");
+    float flotante= scanner. nextFloat();
+
+    System.out.print("Ingrese un caracter: ");
+    char caracter= scanner.next().charAt(0); 
+
+    System.out.print("Fin del programa");
+
+    scanner.nextLine();
+    String cadena= scanner. nextLine();
+    }
+}
