c6712bf44596b36e597c51f0157f9e1e61db3f4f
Jankarlos Crespo <jankarloscrespohdz@gmail.com>
2024-03-26 09:35:37 -0400

    try_catch2

---
+++ Labs C#/Apuntes_If/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        try {
+            
+            int divisor= 0;
+            int result= 10/divisor;
+
+        } catch(DivideBy ZeroExcepcion e){
+           Console.WriteLine(Excepcion capturada: " + e.getMessage());
+      }
+    }
+}
+
