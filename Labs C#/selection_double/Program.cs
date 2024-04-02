70421af8df53afe394a2d183ce11cef5b5f64bfe
Jankarlos Crespo <jankarloscrespohdz@gmail.com>
2024-04-01 23:30:40 -0400

    structure_double

---
+++ Labs C#/selection_double/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program {
+    class Program {
+        static void Main(string[]args) { 
+            Console.Write ("Ingrese su estado (activo/inactivo): ");
+            string answer= Console.ReadLine();
+
+            if(answer == "activo"){
+                Console.WriteLine("Usted puede solicitar prestamos de libros!");
+            }
+            else
+            {
+                Console.WriteLine("Usted no puede solicitar prestamos de libros");
+            }
+        }
+    }
+ }
+
